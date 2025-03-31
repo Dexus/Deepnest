a6e429508c71bc94e526523cd9312cb6403ea82e
Josef Froehle <github@josef-froehle.de>
2025-03-31 23:51:06 +0200

    av

--- forge.config.js
+++ forge.config.js
@@ -67,7 +67,7 @@ const config = {
     ) => {
       try {
         // Execute yarn install with the specified arch in the build folder
-        execSync(`yarn install --immutable`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env, npm_config_target_arch:'arm64' } });
+        execSync(`yarn install --refresh-lockfile`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env, npm_config_target_arch:'arm64' } });
       } catch (error) {
         console.error('Error during yarn install:', error.message);
       }
