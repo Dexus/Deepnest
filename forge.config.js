21a5b673b8c0dae1e981e9a07e3ac82aabba7755
Josef Froehle <github@josef-froehle.de>
2025-04-01 00:51:24 +0200

    av2

--- forge.config.js
+++ forge.config.js
@@ -107,8 +107,8 @@ const config = {
       );
         // Execute yarn install with the specified arch in the build folder
         execSync(`rm -rf yarn.lock node_modules/`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env } });
-        execSync(`ls -alRs`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env } });
-        execSync(`npm install --cpu ${arch} --os ${myPlatform}`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env, npm_config_target_arch:arch } });
+        //execSync(`ls -alRs`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env } });
+        execSync(`npm install --cpu ${arch} --os ${myPlatform}`, { cwd: buildPath, stdio: 'inherit', env: { ...process.env } });
       } catch (error) {
         console.error('Error during npm install:', error.message);
       }
