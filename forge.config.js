a292f992034046d448b4ef71b022f80f3a376f9c
Josef Froehle <github@josef-froehle.de>
2025-03-31 19:07:38 +0200

    mac42

--- forge.config.js
+++ forge.config.js
@@ -69,7 +69,7 @@ const config = {
             const cwd4 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-arm64");
             renameSync(path.resolve(cwd3,'svg-preprocessor.darwin-x64.node'), path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor",'svg-preprocessor.darwin-universal.node'));
             rmSync(cwd3, { recursive: true, force: true });
-            rmSync(cwd3, { recursive: true, force: true });
+            rmSync(cwd4, { recursive: true, force: true });
           } else {
             const cwd3 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-x64");
             const cwd4 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-arm64");
@@ -132,7 +132,7 @@ const config = {
             const cwd4 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-arm64");
             renameSync(path.resolve(cwd3,'svg-preprocessor.darwin-x64.node'), path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor",'svg-preprocessor.darwin-universal.node'));
             rmSync(cwd3, { recursive: true, force: true });
-            rmSync(cwd3, { recursive: true, force: true });
+            rmSync(cwd4, { recursive: true, force: true });
           } else {
             const cwd3 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-x64");
             const cwd4 = path.resolve(buildPath, "node_modules", "@deepnest", "svg-preprocessor-darwin-arm64");
