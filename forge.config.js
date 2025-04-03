dd7ad5a040dbc526b1e3979debd0c7ed05ca28db
Josef Fröhle <github@josef-froehle.de>
2025-04-03 12:44:01 +0200

    hope last time

--- forge.config.js
+++ forge.config.js
@@ -407,9 +407,11 @@ const getMakers = () => {
     },
     {
       name: "@electron-forge/maker-pkg",
-      platforms: ["mas"],
+      platforms: ["darwin", "mas"],
       config: {
-        identity: process.env.APPLE_MAS_INSTALLER_IDENTITY,
+        name: `deepnest-${makerPlatform == 'darwin' ? 'mac' : 'mas'}-${makerArch}`,
+        identity: isMas ? process.env.APPLE_MAS_INSTALLER_IDENTITY : process.env.APPLE_INSTALLER_IDENTITY,
+        appBundleId: "net.deepnest.app",
       },
     },
     {
