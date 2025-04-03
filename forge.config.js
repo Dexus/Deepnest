4b1f39f28ccc344defdde40f128d690b434948a1
Josef Fröhle <github@josef-froehle.de>
2025-04-03 14:56:00 +0200

    one more try 4 - the finale?

--- forge.config.js
+++ forge.config.js
@@ -292,7 +292,7 @@ const getMacSigningConfig = () => {
       identity: isMas ? process.env.APPLE_MAS_IDENTITY : process.env.APPLE_DEVELOPER_ID_APPLICATION,
       entitlements: path.join(__dirname, "_assets", `entitlements${isMas ? '.mas' : ''}.plist`),
       entitlementsInherit: path.join(__dirname, "_assets", `entitlements${isMas ? '.mas' : ''}.inherit.plist`),
-      preAutoEntitlements: true,
+      preAutoEntitlements: false,
       preEmbedProvisioningProfile: true,
       provisioningProfile: path.join(__dirname, "_assets", 'embedded.provisionprofile'),
       optionsForFile: (filePath) => {
