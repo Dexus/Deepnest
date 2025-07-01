c0ab540c9b956fdb9cc9812b494b8b311c275a15
Josef Froehle <github@josef-froehle.de>
2025-07-01 09:18:29 +0200

    test notarize

---
+++ helper_scripts/sign_mac.js
@@ -0,0 +1,15 @@
+import { notarize } from '@electron/notarize';
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return notarize({
+    appPath: `${appOutDir}/${appName}.app`,
+    appleApiIssuer: process.env.APPLE_API_ISSUER,
+    appleApiKey: process.env.APPLE_API_KEY_PATH
+  });
+};
